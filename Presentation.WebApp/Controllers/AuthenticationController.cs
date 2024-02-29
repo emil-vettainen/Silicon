f889d8e6c3fb4d38d6a7ba64677f8067e8ca68ad
emil.vettainen <emil.vettainen@utb.ecutbildning.se>
2024-02-29 01:29:00 +0100

    signup init

--- Presentation.WebApp/Controllers/AuthenticationController.cs
+++ Presentation.WebApp/Controllers/AuthenticationController.cs
@@ -1,10 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Presentation.WebApp.ViewModels;
 
 namespace Presentation.WebApp.Controllers;
 
 public class AuthenticationController : Controller
 {
-   
+
+    [Route("/register")]
+    public IActionResult SignUp()
+    {
+        var viewModel = new AuthenticationViewModel();
+        return View(viewModel);
+    }
+
+    [Route("/register")]
+    [HttpPost]
+    public IActionResult SignUp(AuthenticationViewModel viewModel)
+    {
+        if(ModelState.IsValid)
+        {
+            // registrera 
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        return View(viewModel);
+    }
 
 
 }
