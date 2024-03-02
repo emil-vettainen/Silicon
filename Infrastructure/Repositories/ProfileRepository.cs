8e07823b02d348a071dd106f80393620a835ca57
emil.vettainen <emil.vettainen@utb.ecutbildning.se>
2024-03-02 17:26:20 +0100

    add profilerepository

---
+++ Infrastructure/Repositories/ProfileRepository.cs
@@ -0,0 +1,14 @@
+﻿using Infrastructure.Contexts;
+using Infrastructure.Entities;
+using Shared.Utilis;
+
+namespace Infrastructure.Repositories;
+
+public class ProfileRepository(AccountDbContext context, ErrorLogger errorLogger) : BaseRepository<ProfileEntity, AccountDbContext>(context, errorLogger)
+{
+
+
+
+
+
+}
